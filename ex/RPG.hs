ae61b9741e2033c59af695f613ed54e60602ed29
Lauro Bezerra de Araújo <laurobraxil@gmail.com>
2022-09-07 22:16:17 -0300

    Iniciando atividades 07/09

---
+++ ex/RPG.hs
@@ -0,0 +1,44 @@
+module RPG where
+
+-- Improvise!  A Character could have, for example:
+-- a Name, a Race, a Class, a Level, XP (experience points),
+-- 6 attributes:
+--   Strength Intelligence Wisdom Dexterity Constitution Charisma
+-- current and total HP (hit points)
+-- current and total MP (mana points)
+-- current and total GP (gold pieces)
+data Character =
+   Name | Race | Class | HP | MP | GP | Level | XP
+
+data Attributes = 
+    Strength | Intelligence | Wisdom | Dexterity | Constitution | Charisma
+
+-- does that make sense?
+-- Resp: No, because Character is Data and not a Type that we could handle as a Class.
+type Party = [Character]
+
+-- gets a character and returns one that is the same but +1 level
+gainLevel :: Character -> Character
+gainLevel c = c
+
+
+-- to be used when a character is hit
+hitCharacter :: undefined
+hitCharacter = undefined
+
+alive :: Character -> Bool
+alive = undefined
+
+
+-- How would you implement skills and spells?
+
+data Skill
+
+data Spell
+
+skills :: Character -> [Skill]
+skills = undefined
+
+spells :: Character -> [Spell]
+spells = undefined
+
